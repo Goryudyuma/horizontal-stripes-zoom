74c3d6e25bc9f88380e7880ac87ee617e92b7ae8
GitHub Action <action@github.com>
2022-09-11 09:03:58 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "horizontal-stripes-zoom"
--- src/main/kotlin/com/github/goryudyuma/horizontalstripeszoom/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/goryudyuma/horizontalstripeszoom/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.goryudyuma.horizontalstripeszoom.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.goryudyuma.horizontalstripeszoom.MyBundle
 
 class MyApplicationService {
 
